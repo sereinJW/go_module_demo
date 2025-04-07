442ed6cb412056260581c1056eee1372671a772e
徐伽炜 <1250171739@qq.com>
2025-04-07 20:52:10 +0800

    mathutil包

---
+++ mathutil.go
@@ -0,0 +1,17 @@
+package mathutil
+
+import (
+	"fmt"
+)
+
+func add(a, b int) int {
+	c := a + b
+	fmt.Println("这是两数相加")
+	return c
+}
+
+func Multiply(a, b int) int {
+	c := a * b
+	fmt.Println("这是两数相乘")
+	return c
+}
